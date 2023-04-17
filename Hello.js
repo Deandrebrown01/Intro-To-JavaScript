493ef8e369fd520f0fb18cbec04ca20386c6f307
De'Andre Brown <browndeandre08@yahoo.com>
2023-04-17 14:51:40 -0400

    added Hello.Js and Grade.js

---
+++ Hello.js
@@ -0,0 +1,13 @@
+console.log("Hello")
+let MyName="De'Andre"
+let MyAge=16
+console.log("Hello My Name Is "+ MyName +". My Age Is " + MyAge)
+if (5>1){
+    console.log("Wings are Better than Burgers")
+} else{
+    console.log("Burgers are Better than Wings")
+} 
+let MyList=("Chicken,Fruits, Burgers, Wings,Corn")
+ for (let item of MyList) {
+    console.log(item)
+ }
---
+++ grade.js
@@ -0,0 +1,15 @@
+let MyAverage=100
+console.log("What is your grade average?")
+if (MyAverage=100 && MyAverage>=90){
+    console.log("Your Grade is a A")
+} else if(MyAverage>=80 && MyAverage<=89){
+ console.log("Your Grade is a B")
+}
+if (MyAverage>=70 && MyAverage<79){
+    console.log("Your Grade is a C")
+} else if(MyAverage>=60 && MyAverage<=69){
+    console.log("Your Grade is a D")
+}
+if(MyAverage>=0 && MyAverage<=59){
+    console.log("Your Grade is a F")
+}
