1259cb2f971b840b7267b8140c55bceccd6804f1
De'Andre Brown <browndeandre08@yahoo.com>
2023-05-01 14:45:54 -0400

    gonna add styles soon finished watch

--- stopwatch.js
+++ stopwatch.js
@@ -1,9 +1,42 @@
+
+let milliseconds = 0
+let seconds = 0
+let minutes = 0
+
+let digits = document.getElementById('digits')
+
+let interval;
+
+
 function start(){
-    alert("You clicked the start button")
+  reset()
+
+
+
+    interval= setInterval(function () {
+    milliseconds= milliseconds + 10
+
+    if (milliseconds >= 1000 ){
+        seconds= seconds + 1
+        milliseconds = 0 
+    }
+    if(seconds >=60){
+        minutes= minutes + 1
+       seconds = 0
+    }
+
+    digits.innerHTML = minutes +':'+ String(seconds).padStart(2, '0') + ':' + milliseconds/10
+    },10)
 }
+
 function stop(){
-alert("You clicked the stop button")
+   clearInterval(interval)
+
 }
 function reset(){
-alert("You clicked the reset button")
+stop()
+minutes=0
+seconds=0
+milliseconds=0
+digits.innerHTML=minutes + ':'+ String(seconds).padStart(2,'0') + ':' + String (milliseconds).padStart(2,'0')
 }
